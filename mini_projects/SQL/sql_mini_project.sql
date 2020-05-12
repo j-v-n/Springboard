6fc1b476e7741ac88fe8ca97613c9a4ad2286726
Jayanth Nair <jayanthvasanthnair@gmail.com>
2020-05-11 22:41:27 -0500

    Resubmitted after removing springboard sql login info

--- mini_projects/SQL/sql_mini_project.sql
+++ mini_projects/SQL/sql_mini_project.sql
@@ -1,28 +1,3 @@
-/* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
-following link:
-
-https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
-
-The data you need is in the "country_club" database. This database
-contains 3 tables:
-    i) the "Bookings" table,
-    ii) the "Facilities" table, and
-    iii) the "Members" table.
-
-Note that, if you need to, you can also download these tables locally.
-
-In the mini project, you'll be asked a series of questions. You can
-solve them using the platform, but for the final deliverable,
-paste the code for each solution into this script, and upload it
-to your GitHub.
-
-Before starting with the questions, feel free to take your time,
-exploring the data, and getting acquainted with the 3 tables. */
-
-
 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
